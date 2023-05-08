544ba2c8ac2b49ae0724746b77c722f8173d8bf9
GUAN <jason.li@sunlight-tech.com>
2023-05-08 10:41:09 +0800

    feat: 新建项目

--- scripts/prebuild.mjs
+++ scripts/prebuild.mjs
@@ -14,7 +14,7 @@ let path = `${dir}/docs/.vitepress/dist`
 const gitPullInfo = await $`scp -r ${path}/* root@8.134.174.239://usr/local/web/blog/`
 // const gitPullInfo = await $`cd ${path};`;
 if (gitPullInfo.exitCode === 0) {
-  console.log("复制到linux远程服务器成功")
+  console.log("复制到linux远程服务器成功!")
 } else {
   console.log("发生错误")
 }
